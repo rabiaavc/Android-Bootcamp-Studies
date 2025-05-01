f4cd3b39967bdb2b99f71d0bbbb92447ddec5d5e
rabiaavc <154905533+rabiaavc@users.noreply.github.com>
2025-05-01 17:51:20 +0300

    Add files via upload

---
+++ Homework1.kt
@@ -0,0 +1,139 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+
+    var sehir = "Adana"
+    println("Şehir = $sehir")
+
+    val ulke = "Türkiye"
+    println("Ülke = $ulke")
+
+    var telefon = "0111 222 333"
+    println("Telefon = $telefon")
+
+    val posta_kodu = "010101"
+    println("Posta Kodu = $posta_kodu")
+
+    var email = "example@gmail.com"
+    println("E-mail = $email")
+
+    val meslek = "öğrenci"
+    println("Meslek = $meslek")
+
+    var stok_miktarı = 1000
+    println("Stok Miktarı = $stok_miktarı")
+
+    val musteri_adı = "Rabia"
+    println("Müşteri Adı = $musteri_adı")
+
+    var bakiye = 203400
+    println("Bakiye = $bakiye")
+
+    val dogum_gunu = "04/02/03"
+    println("Doğum Günü = $dogum_gunu")
+
+    var maas = 200000
+    println("Maaş = $maas")
+
+    val medeni_durum = "evli"
+    println("Medeni Durumu = $medeni_durum")
+
+    var urun_yorumu = "Ürünü beğendim. Fiyatı düşünce stok yapıcam."
+    println("ürün Yorumu = $urun_yorumu")
+
+    val odeme_tarihi = "23/04/25"
+    println("Ödeme Tarihi = $odeme_tarihi")
+
+    var odeme = true
+    println("Ödeme = $odeme")
+
+    val siparis_adedi = 1200
+    println("Sipariş Adedi = $siparis_adedi")
+
+    var araba_modeli = 101
+    println("Araba Modeli = $araba_modeli")
+
+    val kitap_adi = "İki Şehrin Hikayesi"
+    println("Kitap Adedi = $kitap_adi")
+
+    var yayinlanma_tarihi = 1859
+    println("Yayınlanma Tarihi = $yayinlanma_tarihi")
+
+    val indirim_miktarı = 10.50
+    println("İndirim Miktarı = $indirim_miktarı")
+
+    var oda_sayisi = 5
+    println("Oda Sayısı = $oda_sayisi")
+
+    val enlem = 10230550
+    println("Enlem = $enlem")
+
+    var boylam = 3480439847
+    println("Boylam = $boylam")
+
+    val urun_adı = "Tablet"
+    println("Ürün Adı = $urun_adı")
+
+    var yemek_fiyati = 440
+    println("Yemek Fiyatı = $yemek_fiyati")
+
+    val marka = "Manuka"
+    println("Marka = $marka")
+
+    var muzik_adi = "RNB"
+    println("Müzik Adı = $muzik_adi")
+
+    val video_suresi = 15.40
+    println("Video Süresi = $video_suresi")
+
+    var urun_puani = 5.0
+    println("Ürün Puanı = $urun_puani")
+
+    val resim_adi = "pantolon"
+    println("Resim Adi = $resim_adi")
+
+    var dosya_formati = "txt"
+    println("Dosya Formatı = $dosya_formati")
+
+    val renk = "siyah"
+    println("Renk = $renk")
+
+    var telefon_modeli = "note 11 pro"
+    println("Telefon Adı = $telefon_modeli")
+
+    val ekran_boyutu = 12.5
+    println("Ekran Boyutu = $ekran_boyutu inc")
+
+    var agırlık = 3.5
+    println("Ağırlık = $agırlık kg")
+
+    val ulusal_gun = "23 Nisan"
+    println("Ulusal Gün = $ulusal_gun")
+
+    var tatil_günü = "Çarşamba"
+    println("Tatil Günü = $tatil_günü")
+
+    val rezervasyon_tarihi = "26/04/2025"
+    println("Rezervasyon Tarihi = $rezervasyon_tarihi")
+
+    var sokak_adı = 531
+    println("Sokak Adı = $sokak_adı")
+
+    val otobus_hatti = "500T"
+    println("Otobüs Hattı = $otobus_hatti")
+
+    var kalan_dakika = 50
+    println("Kalan Dakika = $kalan_dakika saniye")
+
+    val takip_kodu = 897654
+    println("Takip Kodu = $takip_kodu")
+
+    var kupon_suresi = "2 gün"
+    println("Kupon Süresi = $kupon_suresi")
+
+    val kupon_kodu = "22NISAN2025"
+    println("Kupon Kodu = $kupon_kodu")
+
+    var fatura_adresi = "Elektrik İdaresi"
+    println("Fatura Adresi = $fatura_adresi")
+}
